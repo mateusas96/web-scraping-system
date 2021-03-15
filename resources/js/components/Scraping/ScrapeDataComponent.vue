52071f3ac4e4d5dec9e68bd377771cd01c0035c4
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2021-03-15 19:48:23 +0200

    bug fix, improvements

--- resources/js/components/Scraping/ScrapeDataComponent.vue
+++ resources/js/components/Scraping/ScrapeDataComponent.vue
@@ -698,6 +698,7 @@ export default {
                 {name: 'Daily (at 6AM)', value: 'daily'},
                 {name: 'Weekly (every Monday at 6AM)', value: 'weekly'},
             ],
+            windowWidth: 0,
         }
     },
     mounted() {
@@ -707,6 +708,7 @@ export default {
         Fire.$on('searchMyFile', () => {
             this.getMyFiles();
         });
+        this.windowWidth = window.innerWidth;
     },
     destroyed() {
         hideScrollbar();
