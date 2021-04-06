c239b974ff241477a951dcc6941560eea7f9bcf6
Matas Mikušauskas <45999356+mateusas96@users.noreply.github.com>
2021-04-06 21:50:37 +0300

    valid password unit test

---
+++ tests/Unit/PasswordTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Rules\isPasswordValid;
+
+class PasswordTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Valid password requirements:
+     * - one uppercase letter
+     * - one number
+     * - one special character
+     */
+
+    /**
+     * Testing password only with lower case letters
+     *
+     * @return void
+     */
+    public function test_password_only_with_lowercase_letters()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'slaptazodis');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_one_number()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'slaptazodis1');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_uppercase_letter()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'Slaptazodis');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_one_special_char_and_one_uppercase_letter()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'Slaptazodis#');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_one_special_char_and_one_number()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'Slaptazodis2');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_one_uppercase_letter()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'slaptazodis#');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_one_number_and_one_special_char()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return false because password does not meet requirements
+        $response = $isPasswordValid->passes('password', 'slaptazodis1#');
+
+        $this->assertEquals($response, false);
+    }
+
+    public function test_password_with_lowercase_letters_and_one_number_and_one_special_char_and_one_uppercase_letter()
+    {
+        $isPasswordValid = new isPasswordValid();
+
+        // response must return true because password meets requirements
+        $response = $isPasswordValid->passes('password', 'Slaptazodis1#');
+
+        $this->assertEquals($response, true);
+    }
+}
